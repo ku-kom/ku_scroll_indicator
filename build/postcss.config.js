7e9af0989596b61b7cf21fb98f13a5dc55cb8ade
nanna-dk <nanna@e-nanna.dk>
2022-07-13 13:44:07 +0200

    Init

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,21 +1,23 @@
 'use strict'
 
 const mapConfig = {
-  inline: false,
-  annotation: true,
-  sourcesContent: true
+    inline: false,
+    annotation: true,
+    sourcesContent: true
 }
 
 module.exports = context => {
-  return {
-    map: context.file.dirname.includes('examples') ? false : mapConfig,
-    plugins: {
-      autoprefixer: {
-        cascade: false
-      },
-      cssnano: {
-        preset: 'default',
-      },
+    return {
+        map: mapConfig,
+        plugins: {
+            autoprefixer: {
+                cascade: false
+            },
+            cssnano: {
+                preset: [
+                    'default'
+                ]
+            },
+        }
     }
-  }
 }
